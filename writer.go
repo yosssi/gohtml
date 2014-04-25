50a677506006e0b4b69afb5c6eaef09fe10be375
yosssi <yoshida.keiji.84@gmail.com>
2014-04-25 16:46:13 +0900

    Added comments on writer.go.

--- writer.go
+++ writer.go
@@ -5,17 +5,20 @@ import (
 	"io"
 )
 
+// A Writer represents a formatted HTML source codes writer.
 type Writer struct {
 	writer      io.Writer
 	lastElement string
 	bf          *bytes.Buffer
 }
 
+// SetLastElement set the lastElement to the Writer.
 func (wr *Writer) SetLastElement(lastElement string) *Writer {
 	wr.lastElement = lastElement
 	return wr
 }
 
+// Write writes the parameter.
 func (wr *Writer) Write(p []byte) (n int, err error) {
 	wr.bf.Write(p)
 	if bytes.HasSuffix(p, []byte(wr.lastElement)) {
@@ -24,6 +27,7 @@ func (wr *Writer) Write(p []byte) (n int, err error) {
 	return 0, nil
 }
 
+// NewWriter generates a Writer and returns it.
 func NewWriter(wr io.Writer) *Writer {
 	return &Writer{writer: wr, lastElement: defaultLastElement, bf: &bytes.Buffer{}}
 }
