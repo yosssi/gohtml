6e1a12d513325b77e968027671f678b69b9728e3
yosssi <yoshida.keiji.84@gmail.com>
2014-04-25 22:24:07 +0900

    Updated formatter.go.

--- formatter.go
+++ formatter.go
@@ -13,7 +13,11 @@ func Format(s string) string {
 
 // Format parses the input HTML string, formats it and returns the result with line no.
 func FormatWithLineNo(s string) string {
-	lines := strings.Split(Format(s), "\n")
+	return AddLineNo(Format(s))
+}
+
+func AddLineNo(s string) string {
+	lines := strings.Split(s, "\n")
 	maxLineNoStrLen := len(strconv.Itoa(len(lines)))
 	bf := &bytes.Buffer{}
 	for i, line := range lines {
@@ -24,4 +28,5 @@ func FormatWithLineNo(s string) string {
 		bf.WriteString(strings.Repeat(" ", maxLineNoStrLen-len(lineNoStr)) + lineNoStr + "  " + line)
 	}
 	return bf.String()
+
 }
